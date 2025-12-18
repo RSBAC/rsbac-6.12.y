b69792ab9c475bf7b9a187d84332b3157b244e95
Amon Ott <ao@rsbac.org>
2025-12-18 14:40:58 +0100

    Net scheduler fixes: move GET_STATUS_DATA, fix __tc_dump_tclass() error path

--- net/sched/sch_api.c
+++ net/sched/sch_api.c
@@ -1569,6 +1569,7 @@ static int tc_get_qdisc(struct sk_buff *skb, struct nlmsghdr *n,
 				rsbac_target_id,
 				A_none,
 				rsbac_attribute_value)) {
+		NL_SET_ERR_MSG(extack, "Permission denied");
 		return -EPERM;
 	}
 #endif
@@ -1703,6 +1704,7 @@ replay:
 				rsbac_target_id,
 				A_none,
 				rsbac_attribute_value)) {
+		NL_SET_ERR_MSG(extack, "Permission denied");
 		return -EPERM;
 	}
 #endif
@@ -2528,7 +2530,7 @@ static int tc_dump_tclass(struct sk_buff *skb, struct netlink_callback *cb)
 				rsbac_target_id,
 				A_none,
 				rsbac_attribute_value)) {
-		dev_put(dev);
+		cb->args[0] = 0;
 		return -EPERM;
 	}
 #endif
